a08968f4f2c8fc4b88c627c62d45a0130112fc80
Zain Ali <67614246+zainali1234@users.noreply.github.com>
2022-07-13 18:11:28 -0500

    Update input_predictions.py

--- input_predictions.py
+++ input_predictions.py
@@ -1,10 +1,16 @@
 from model_training import *
 import pandas as pd
 
+#creates a dataframe using the .csv dataset
 original_df = create_df()
+
+#creates an empty copy of the dataframe for user input
 input_df = original_df[0:0]
+
+#Creates and fits the model using the dataset
 my_pipeline = create_model(original_df)
 
+#Prompts user for input
 new_row = {'GREScore': [float(input("Enter GREScore: "))],
            'TOEFLScore': [float(input("Enter TOEFLScore: "))],
            'UniRating': [float(input("Enter UniRating: "))],
@@ -13,20 +19,25 @@ new_row = {'GREScore': [float(input("Enter GREScore: "))],
            'CGPA': [float(input("Enter CGPA: "))],
            'Research': [float(input("Enter Research: "))]}
 
+#adds user-input row of data to the input df for predictions
 row_df = pd.DataFrame.from_dict(new_row)
 input_df = pd.concat([input_df, row_df])
 
 dataCols = ['GREScore', 'TOEFLScore', 'UniRating', 'SOP',
                               'LOR', 'CGPA', 'Research']
 
+#removes AdmissionChance column since it is the prediction target
 original_df = original_df.drop("AdmissionChance", axis=1)
 input_df = input_df.drop("AdmissionChance", axis=1)
 
+#creates and stores prediction using user-input values
 preds = round(((my_pipeline.predict(input_df))[0] * 100), 3)
 
+#prints predictions to user
 print("You have a " + str(preds) + "% chance of being admitted to a university with a rating of "
       + str(new_row['UniRating'][0]) + ".")
 
+#finds and outputs the mean difference between the input data and imported dataset
 original_means = original_df[dataCols].mean()
 input_df_means = input_df[dataCols].mean()
 
